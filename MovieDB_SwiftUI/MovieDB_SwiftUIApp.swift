d7e42c13846720f2d2b3d019b637c6f35a0a3a1f
Hugo Pinheiro <hugo_hp10@hotmail.com>
2023-05-21 18:53:19 -0300

    Initial Commit

---
+++ MovieDB_SwiftUI/MovieDB_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDB_SwiftUIApp.swift
+//  MovieDB_SwiftUI
+//
+//  Created by HP on 21/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieDB_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
